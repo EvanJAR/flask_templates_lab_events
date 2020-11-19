db760e2549bfcae880a1a6bf74094d7300b946e9
Evan Reid <evan_1995@hotmail.com>
2020-11-19 16:20:18 +0000

    first extension

--- app/models/events_list.py
+++ app/models/events_list.py
@@ -1,6 +1,7 @@
 from app.models.event import *
 
-event_1 = Event("25th December 2020", "Big Christmas Shindig", 2, "House", "Celebrating Christmas from the comfort of your home, family on zoom")
-event_2 = Event("31st December 2020", "New Years Eve Extravaganza", 2, "Kitchen", "Celebrating New Year's Eve from the comfort of your home, friends on zoom")
+event_1 = Event("25th December 2020", "Big Christmas Shindig", 2, "House", "Celebrating Christmas from the comfort of your home, family on zoom", True)
+event_2 = Event("31st December 2020", "New Years Eve Extravaganza", 2, "Kitchen", "Celebrating New Year's Eve from the comfort of your home, friends on zoom", True)
+event_3 = Event("28th December 2020", "Big Milk Buying Trip For Giant Cup of Tea", 1, "kitchen", "obvious", False)
 
-events = [event_1, event_2]
+events = [event_1, event_2, event_3]
