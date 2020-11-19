52b59f52a350c13dd6771970cd8d174534660e8c
Evan Reid <evan_1995@hotmail.com>
2020-11-19 15:54:30 +0000

    MVP complete

--- app/controller.py
+++ app/controller.py
@@ -5,3 +5,7 @@ from app.models.events_list import events
 @app.route('/')
 def index():
     return render_template('index.html', title='Home', events=events)
+
+@app.route('/planner')
+def planner():
+    return render_template('event_planner.html', title='Planner', events=events)
