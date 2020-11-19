b7166bc41167ce9152b8fa17e48dd83e3cb0a09b
Evan Reid <evan_1995@hotmail.com>
2020-11-19 15:18:52 +0000

    event class and events list

---
+++ app/models/events_list.py
@@ -0,0 +1,6 @@
+from app.models.event import *
+
+event_1 = Event("25th December 2020", "Big Christmas Shindig", 2, "House", "Celebrating Christmas from the comfort of your home, family on zoom")
+event_2 = Event("31st December 2020", "New Years Eve", 2, "Kitchen", "Celebrating New Year's Eve from the comfort of your home, friends on zoom")
+
+events = [event_1, event_2]
